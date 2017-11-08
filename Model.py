93ce0e7b14fcf279c2d1799285a1ff711dfe2ac5
Sasha Popov <Sasha@s172-20-50-h164.paws.uga.edu>
2017-11-07 19:49:58 -0500

    Setup data splitting

--- Model.py
+++ Model.py
@@ -1,5 +1,10 @@
 from keras import Sequential
 from keras.layers import Dense, LSTM, Dropout, Embedding
+from keras.preprocessing import sequence
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 def build_model():
@@ -25,3 +30,40 @@ def build_model():
         optimizer="adam",
         metrics=["binary_accuracy", 'categorical_crossentropy']
     )
+
+    return model
+
+
+def train_model():
+
+    max_size = 5000
+
+    data = pd.read_csv("CleanedReviews.csv")
+
+    X_data = data['Test']
+    Y_temp = data['Score']
+    Y_data = []
+
+    X_data = sequence.pad_sequences(X_data, maxlen=max_size)
+
+    for val in Y_temp:
+
+        if val == 1:
+            Y_data.append([1, 0, 0, 0])
+        elif val == 2:
+            Y_data.append([0, 1, 0, 0])
+        elif val == 4:
+            Y_data.append([0, 0, 1, 0])
+        else:
+            Y_data.append([0, 0, 0, 1])
+
+    x_train, x_test, y_train, y_test = train_test_split(X_data, Y_data, test_size=0.3)
+
+    model = build_model()
+
+    history = model.fit(x_train, y_train, batch_size=32, epochs=1)
+
+    plt.plot(history.history["binary_accuracy"])
+    plt.plot(history.history["categorical_crossentropy"])
+
+    plt.show()
